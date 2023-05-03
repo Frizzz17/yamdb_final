298d45440fba0c3f79011b343624a4f93b9f8fe6
Андрей Фризен <frizen_a@inbox.ru>
2023-05-03 16:56:52 +0700

    flak

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -4,7 +4,7 @@ from datetime import timedelta
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-SECRET_KEY = "kf_(s9f^@9a2t6z_u%8f&vaia&n61h+@d-_alq@_0slp*)9fqs" # Без ключа не проходит тесты
+SECRET_KEY = "kf_(s9f^@9a2t6z_u%8f&vaia&n61h+@d-_alq@_0slp*)9fqs"  # Без ключа не проходит тесты
 
 
 DEBUG = False
